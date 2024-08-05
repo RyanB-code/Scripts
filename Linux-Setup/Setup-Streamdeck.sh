f0eb420aba23620ed2f813c875ecb2f8dd318673
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-08-04 19:13:30 -0700

    Downloads and installs python packages

--- Linux-Setup/Setup-Streamdeck.sh
+++ Linux-Setup/Setup-Streamdeck.sh
@@ -28,6 +28,18 @@ else
     exit 2
 fi
 
+if python3 -m pip install --upgrade pip 1>/dev/null 2>&1; then
+    echo "Successfully installed python packages"
+else
+    echo "Could not install python packages."
+    echo "Try running the script with sudo privileges."
+    exit 2
+fi
+
+
+# ==========================================
+# Final note
+echo ""
 echo "Download successful."
 
 
