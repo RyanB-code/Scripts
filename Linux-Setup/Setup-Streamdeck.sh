16cbcefe9b7a6ff122c012cdf20f5e68dcc7e34b
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-08-09 22:07:22 -0700

    Updated install script to copy of one from GitHub

--- Linux-Setup/Setup-Streamdeck.sh
+++ Linux-Setup/Setup-Streamdeck.sh
@@ -1,45 +1,13 @@
-#!/bin/bash
+#!/bin/bash -xe
+echo "Installing libraries"
+sudo apt install python3-pip libhidapi-libusb0 libxcb-xinerama0
 
+echo "Adding udev rules and reloading"
+sudo tee /etc/udev/rules.d/70-streamdeck.rules << EOF
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", TAG+="uaccess"
+KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess"
+EOF
+sudo udevadm trigger
 
-# Assumptions
-#   1. Run with sudo privileges
-
-if apt install streamdeck-ui 1>/dev/null 2>&1; then
-    echo "Successfully installed streamdeck-ui."
-else
-    echo "Could not install streamdeck-ui."
-    echo "Try running the script with sudo privileges."
-    exit 2
-fi
-
-if apt install libxcb-xinerama0 1>/dev/null 2>&1; then
-    echo "Successfully installed libxcb-xinerama0."
-else
-    echo "Could not install libxcb-xinerama0."
-    echo "Try running the script with sudo privileges."
-    exit 2
-fi
-
-if apt install libxcb-cursor0 1>/dev/null 2>&1; then
-    echo "Successfully installed libxcb-xinerama0."
-else
-    echo "Could not install libxcb-cursor0."
-    echo "Try running the script with sudo privileges."
-    exit 2
-fi
-
-if python3 -m pip install --upgrade pip 1>/dev/null 2>&1; then
-    echo "Successfully installed python packages"
-else
-    echo "Could not install python packages."
-    echo "Try running the script with sudo privileges."
-    exit 2
-fi
-
-
-# ==========================================
-# Final note
-echo ""
-echo "Download successful."
-
-
+pip3 install --user streamdeck-linux-gui
+echo "If the installation was successful, run 'streamdeck' to start."
