a4f8cc7cdbf14c237f0f9091a5e13c0c4b396622
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2025-08-23 11:21:06 -0700

    Updated script to use tmux for all panes

--- open-dev-env/open-dev-env.sh
+++ open-dev-env/open-dev-env.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "/opt/nvim-linux-x86_64/bin/nvim /mnt/NAS/1-Project-Related/Project-Source-Directories/ ; exec bash"
+cd ~
 
-tmux new-session -d -s Development 'time-tracker ; bash -i' \; new-window 'cd /mnt/NAS/1-Project-Related/Project-Source-Directories/; bash -i' \;
-tmux attach-session -t Development
-
+tmux new-session -d -s Development -n 'time-tracker' 'time-tracker ; bash -i'
+tmux new-window -t Development:1 -n 'Directory' 'cd /mnt/NAS/1-Project-Related/Project-Source-Directories/; bash -i'
+tmux new-window -t Development:2 -n 'nvim' '/opt/nvim-linux-x86_64/bin/nvim /mnt/NAS/1-Project-Related/Project-Source-Directories/ ; bash -i' 
+tmux attach-session -t Development 
 
